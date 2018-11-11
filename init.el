3450309dfe214a19cd66fdf9188644f7875748ef
Jed Clinger <jed.clinger@gmail.com>
2018-11-11 11:17:51 -0800

    Add org-tree-slide

--- init.el
+++ init.el
@@ -343,6 +343,12 @@ packages are already installed which improves startup time."
   :init
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
+(use-package org-tree-slide :ensure t :defer t
+  :bind
+  (("<f8>" . org-tree-slide-mode)
+   ("<f9>" . org-tree-slide-move-previous-tree)
+   ("<f10>" . org-tree-slide-move-next-tree)))
+
 (use-package projectile :ensure t :defer t)
 
 (use-package helm-projectile
